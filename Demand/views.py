29007a284fa82a1f6526a3f3b21537868714335f
ViniciusCarvalhoDevy <vinicius.b.carvalho18@gmail.com>
2025-09-03 13:37:30 -0300

    initial register demand

--- Demand/views.py
+++ Demand/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import FormView
 from .forms import DemandFormModel
+from django.http import HttpResponse
 
 # Create your views here.
 class DemandRegisterView(FormView):
@@ -9,9 +10,14 @@ class DemandRegisterView(FormView):
     form_class = DemandFormModel
     
     def form_valid(self, form):
-        pass
-        return super().form_valid(form)
+        try:
+            forms = DemandFormModel(self.request.POST)
+            forms.save(commit=False)
+            print(forms.cleaned_data.POST)
+        except Exception as e:
+            e.add_note("Erro em salvar dados para a inclusão da Demanad")
+        return HttpResponse('Demanda cadastrada com sucesso!')
     
     def form_invalid(self, form):
         pass
-        return super().form_invalid(form)
+        return render(self.request, 'demandRegister.html', {'form': form})
